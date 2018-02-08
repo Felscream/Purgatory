dac37001d5b154703822ee9c884a2da181bdbf0f
nguessan-pierre <n.pierre.alex@gmail.com>
2018-02-07 21:03:36 -0500

    game manager + test item

---
+++ Assets/Scripts/Utility/ManagerInGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerInGame : MonoBehaviour {
+
+	public GameObject Health;
+	public GameObject Stamina;
+	public GameObject Orb;
+	public GameObject Plateform;
+	private float Timer = 0.0f;
+	private bool SpawnHealth;
+	private bool SpawnStamina;
+	private bool SpawnOrb;
+	//private bool SpawnPlateform;
+	//private int PlayerAlive;
+
+	void Update () {
+		Timer += Time.deltaTime;
+
+		//Apparition des items sur la map à partir des prefabs
+		if (Timer >= 5 && !SpawnHealth) {
+			Instantiate (Health);
+			SpawnHealth = true;
+		}
+
+		if (Timer >= 20 && !SpawnOrb) {
+			Instantiate (Orb);
+			Instantiate (Plateform);
+			SpawnOrb = true;
+		}
+
+		if (Timer >= 10 && !SpawnStamina) {
+			Instantiate (Stamina);
+			SpawnStamina = true;
+		}
+
+		/*if (PlayerAlive == 1) {
+			
+		}*/
+	}
+}
