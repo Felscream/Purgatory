b452b297dc4e5bdd1f6bef33f336965441124b74
nguessan-pierre <n.pierre.alex@gmail.com>
2018-02-22 17:46:40 -0500

    fix gamemanager

--- Assets/Scripts/Utility/ManagerInGame.cs
+++ Assets/Scripts/Utility/ManagerInGame.cs
@@ -8,57 +8,22 @@ public class ManagerInGame : MonoBehaviour {
 	public GameObject Health;
 	public GameObject Stamina;
 	public GameObject Orb;
-	public GameObject Plateform;
 	private float Timer = 0.0f;
 	private bool SpawnHealth;
 	private bool SpawnStamina;
 	private bool SpawnOrb;
-	private PlayerHealthSlider _playerhealth;
-	/*public Champion player1;
-	public Champion player2;
-	public Champion player3;
-	public Champion player4;
-	public bool playerAlive1 = false;
-	public bool playerAlive2 = false;
-	public bool playerAlive3 = false;
-	public bool playerAlive4 = false;*/
-	//private bool SpawnPlateform;
 	private int PlayerAlive=0;
 	public Component[] Players;
 
 
 	void Awake(){
 		Players = GetComponentsInChildren<Champion>();
-		foreach (Champion player in Players) {
-			if (player != null) {
-				PlayerAlive += 1;
-				Debug.Log ("test concluant");
-			}
-		}
-		Debug.Log (PlayerAlive);
-		/*
-		if (player1 != null) {
-			PlayerAlive+=1 ;
-			playerAlive1 = true;
-		}
-		if (player2 != null) {
-			PlayerAlive+=1 ;
-			playerAlive2 = true;
-		}
-		if (player3 != null) {
-			PlayerAlive+=1 ;
-			playerAlive3 = true;
-		}
-		if (player4 != null) {
-			PlayerAlive+=1 ;
-			playerAlive4 = true;
-		}*/
-		Debug.Log("fin Awake");
 	}
+
 	void Update () {
+		PlayerAlive = 0;
 		Timer += Time.deltaTime;
-		//Debug.Log (player1.Health);
-		//Debug.Log (player2.Health);
+
 		//Apparition des items sur la map à partir des prefabs
 		if (Timer >= 5 && !SpawnHealth) {
 			Instantiate (Health);
@@ -78,56 +43,15 @@ public class ManagerInGame : MonoBehaviour {
 
 		foreach (Champion player in Players) {
 			if (player != null) {
-				if (player.Health <= 0) {
-					PlayerAlive -= 1;
-					//Destroy(player);
-					//manage playeralive pour enlever 1 fois/perso
-					//player.gameObject.;
-					Debug.Log (PlayerAlive);
+				if (player.Health > 0) {
+					PlayerAlive += 1;
 				}
 			}
 		}
-
-
-		/*if (player1 != null)
-		{
-			if (player1.Health <= 0 && playerAlive1) {
-				PlayerAlive -= 1;
-				playerAlive1 = false;
-			}
-		}
-
-		if (player2 != null)
-		{
-			if (player2.Health <= 0 && playerAlive2) {
-				PlayerAlive -= 1;
-				playerAlive2 = false;
-				Debug.Log ("bonjour");
-			}
-		}
-
-		if (player3 != null)
-		{
-			if (player3.Health <= 0 && playerAlive3) {
-				PlayerAlive -= 1;
-				playerAlive3 = false;
-			}
-		}
-
-		if (player4 != null)
-		{
-			if (player4.Health <= 0 && playerAlive4) {
-				PlayerAlive -= 1;
-				playerAlive4 = false;
-			}
-		}*/
-
-
+			
 		if (PlayerAlive == 1) {
 			SceneManager.LoadScene (1);
-			//Ajout de la suite
+			//ici ajouter le changement de scène et toute les modifs à prendre en compte
 		}
-
-		//Debug.Log (PlayerAlive);
 	}
 }
