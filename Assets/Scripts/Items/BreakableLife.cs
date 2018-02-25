f838338df814f47eadfa3a422d931b98373df7ee
louis2580 <louis0710@live.fr>
2018-02-24 22:03:08 -0500

    réhaussement du sol pour faire passer les HUD des players 3 et 4 dessous pour ne pas géner flash rouge du levier quand on le tape 2 ou 3 ajustements sur les prefabs

--- Assets/Scripts/Items/BreakableLife.cs
+++ Assets/Scripts/Items/BreakableLife.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class BreakableLife : MonoBehaviour {
 
-    public int life = 4;
+    public int maxLife = 4;
+    private int life = 4;
     public Transform ascendant;
     public Transform descendant;
     public Transform falling;
     private bool    hasFallen = false,
                     isLowest = false,
                     beginLowering = false;
+    private bool damaged = false;
+    private Color initialColor;
+    private SpriteRenderer leverColor;
 
     // Timer to low the plateform
     private float timer;
@@ -19,7 +23,9 @@ public class BreakableLife : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        initialColor = new Color(0, 255, 255);
+        leverColor = this.gameObject.GetComponent<SpriteRenderer>();
+        leverColor.color = initialColor;
         ascendant.gameObject.SetActive(true);
         descendant.gameObject.SetActive(true);
         falling.gameObject.SetActive(false);
@@ -32,7 +38,7 @@ public class BreakableLife : MonoBehaviour {
         if (!beginLowering)
         {
             timer += Time.deltaTime;
-            if (timer >= 5) // Ammorce la descente après 5 secondes
+            if (timer >= 8) // Ammorce la descente après 5 secondes
             {
                 beginLowering = true;
                 this.transform.rotation = new Quaternion(0, 0, 0, 0);
@@ -45,23 +51,35 @@ public class BreakableLife : MonoBehaviour {
             {
                 timer += Time.deltaTime;
                 ascendant.localPosition = new Vector3(1, Random.value * 0.1f, 0);
-                descendant.localPosition = new Vector3(0, 10 - timer, 0);            // Abaisse la plateforme jusqu'à un offset vertical de 0
+                descendant.localPosition = new Vector3(0, 10 - timer/2, 0);            // Abaisse la plateforme jusqu'à un offset vertical de 0
             }
             if (descendant.position.y <= 0)
             {
                 isLowest = true;
             }
         }
+        if (damaged)
+        {
+            leverColor.color = new Color(255, 0, 0);
+        } else
+        {
+            leverColor.color = Color.Lerp(leverColor.color, initialColor, .8f);
+        }
+        damaged = false;
 	}
 
     public void TakeDamage(int dmg)
     {
-        Debug.Log("Take damage");
-        life -= dmg;
-        if (life <= 0)
-        {
-            Fall();
-            hasFallen = true;
+        if (beginLowering)
+        { 
+            Debug.Log("Take damage");
+            life -= dmg;
+            if (life <= 0)
+            {
+                Fall();
+                hasFallen = true;
+            }
+            damaged = true;
         }
     }
 
@@ -72,6 +90,7 @@ public class BreakableLife : MonoBehaviour {
         ascendant.gameObject.SetActive(false);
         descendant.gameObject.SetActive(false);
         this.gameObject.SetActive(false);
+        Destroy(falling.gameObject, 5f);
         
     }
 }
