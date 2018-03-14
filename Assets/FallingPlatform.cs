59398268a73c8cacf2e96661dcf2cc5033e57bf3
louis2580 <louis0710@live.fr>
2018-03-14 13:19:48 -0400

    La plateforme détecte bien le champion sur lequel elle tombe mais ne lui inflige pas de dégât... pourquoi? à fixer

--- Assets/FallingPlatform.cs
+++ Assets/FallingPlatform.cs
@@ -7,28 +7,24 @@ public class FallingPlatform : MonoBehaviour {
     public float height = -1.8f; //lowest position
     private bool canDealDamage = true;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (transform.position.y <= -3.5) // on the ground
-        {
-            canDealDamage = false;
-        }
-	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         float facing = collision.transform.position.x.CompareTo(0); // -1 if <0, 0 if ==0 and 1 if >0
         int damage = (int) (height - collision.transform.position.y)*10;
         Vector2 recoil = new Vector2(facing * 5, -1);
-        Debug.Log(damage);
-        if (canDealDamage && collision.gameObject.GetComponent<Champion>() != null)
+        Champion champion = collision.GetComponent<Champion>();
+        Debug.Log(transform.position.y +" "+ collision.name);
+
+        if (canDealDamage && champion != null)
         {
-            collision.gameObject.GetComponent<Champion>().ApplyDamage(damage, facing, 0, recoil, true);
+            Debug.Log(damage + " ici " + facing);
+            champion.ApplyDamage(damage, facing, 0, recoil, true);
+        }
+
+        if (collision.tag == "obstacle") // on the ground
+        {
+            canDealDamage = false;
         }
     }
 }
