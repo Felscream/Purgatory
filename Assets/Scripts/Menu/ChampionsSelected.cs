d4fe3b0ff34d759a1b8bc9c5bd7b6c9ba9d2f3ac
aperro <adrien.perraud@telecom-st-etienne.fr>
2018-03-26 15:51:07 -0400

    selectionPlayer presque ok

--- Assets/Scripts/Menu/ChampionsSelected.cs
+++ Assets/Scripts/Menu/ChampionsSelected.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChampionsSelected : MonoBehaviour {
 
     private static ChampionsSelected instance;
     //private int player1_indexSelection = 0, player2_indexSelection = 0, player3_indexSelection = 0, player4_indexSelection = 0;
     private int playerNumber;
+
+    public string Start_P1;
+    private bool lobbySelected = false;
 
     public List<int> playerSelection = new List<int>();
 
@@ -32,9 +36,14 @@ public class ChampionsSelected : MonoBehaviour {
         }
     }
 
-    public void ReOrganize()
+    public void LoadLevel()
     {
-
+        if (Input.GetButtonDown(Start_P1) && !lobbySelected)
+        {
+            Debug.Log("Je passe ici");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            lobbySelected = true;
+        }
     }
 
     public static ChampionsSelected GetInstance()
